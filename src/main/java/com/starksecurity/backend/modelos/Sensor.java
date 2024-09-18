967ae77c807a2d434ce59d6487fcc82b58574d4c
Augusto <sssdark302@gmail.com>
2024-09-18 17:36:50 +0200

    feat: modelo-sensor

---
+++ src/main/java/com/starksecurity/backend/modelos/Sensor.java
@@ -0,0 +1,51 @@
+package com.starksecurity.backend.modelos;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sensores")
+public abstract class Sensor {
+    @Id
+    @SequenceGenerator(
+            name = "secuencia_sensor",
+            sequenceName = "secuencia_sensor",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "secuencia_sensor"
+    )
+    private long id;
+    @Getter @Setter
+    private String nombre, ubicacion;
+    @Getter @Setter
+    private boolean estado;
+
+    public Sensor() {
+    }
+    public Sensor(String nombre, String ubicacion, boolean estado) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+    }
+    public Sensor(long id, String nombre, String ubicacion, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+    }
+    public abstract void detectar(String valor); // detecta un valor y lo almacena en la base de datos
+
+
+    @Override
+    public String toString() {
+        return "Sensor[" +
+                "Id: " + id +
+                ", Nombre: " + nombre + '\'' +
+                ", Ubicacion: " + ubicacion + '\'' +
+                ", Estado:" + estado +
+                ']';
+    }
+}
