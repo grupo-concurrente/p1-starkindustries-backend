5a4f6fbb8c8a0f8e6f6cdb25c74dcb408fd7bb26
Augusto <sssdark302@gmail.com>
2024-09-18 17:15:25 +0200

    feat: modelo-usuario

--- src/main/java/Enums/Rol.java
+++ src/main/java/Enums/Rol.java
@@ -1,4 +1,4 @@
-package usuario;
+package Enums;
 
 public enum Rol {
     ADMINISTRADOR,
--- src/main/java/com/starksecurity/backend/BackendApplication.java
+++ src/main/java/com/starksecurity/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.starksecurity.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
