15e44aedbcab361af838d597c22cfa82dd28a72e
Augusto <sssdark302@gmail.com>
2024-09-19 12:00:49 +0200

    fix: controlador-usuario

--- src/main/java/com/starksecurity/backend/controladores/ControladorUsuario.java
+++ src/main/java/com/starksecurity/backend/controladores/ControladorUsuario.java
@@ -1,5 +1,6 @@
 package com.starksecurity.backend.controladores;
 
+import com.starksecurity.backend.modelos.Rol;
 import com.starksecurity.backend.modelos.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("api/v1/usuarios")
 public class ControladorUsuario {
     @GetMapping
     public List<Usuario> getUsuarios() {
         return List.of(
-                new Usuario(1, "email1", "nombre1", "contrasena1", null));
+                new Usuario[]{
+                        new Usuario(1, "email1", "user1", "contrasena1", Rol.USUARIO),
+                        new Usuario(2, "email2", "admin1", "contrasena2", Rol.ADMINISTRADOR),
+                }
+        );
     }
 }
